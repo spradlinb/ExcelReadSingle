58d40100af1d8cf3946bdf854c7a8ebfcbe1bb48
spradlinb <spradlinb@gmail.com>
2014-11-19 21:30:48 -0600

    Initial Sync

---
+++ ExcelReadSingle/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ExcelReadSingle
+{
+    partial class ExcelProcessor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.cellValue = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(13, 13);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(95, 15);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(424, 20);
+            this.filePath.TabIndex = 1;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(444, 40);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 2;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // cellValue
+            // 
+            this.cellValue.Location = new System.Drawing.Point(13, 43);
+            this.cellValue.Name = "cellValue";
+            this.cellValue.Size = new System.Drawing.Size(425, 20);
+            this.cellValue.TabIndex = 3;
+            // 
+            // ExcelProcessor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 83);
+            this.Controls.Add(this.cellValue);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.browseButton);
+            this.Name = "ExcelProcessor";
+            this.Text = "ExcelProcessor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.TextBox cellValue;
+    }
+}
+
---
+++ ExcelReadSingle/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelReadSingle
+{
+    public partial class ExcelProcessor : Form
+    {
+        private Row[] _rows = null;
+        private int _rowMax = 0;
+        private int _colMax = 0;
+        private int _rowCount = 0;
+        private int _colCount = 0;
+
+        public ExcelProcessor()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            if (_rowMax != 0 && _colMax != 0)
+            {
+                _rowMax = 0;
+                _colMax = 0;
+                _rowCount = 0;
+                _colCount = 0;
+                nextButton.Enabled = true;
+            }
+
+            openFileDialog1.Filter = "Excel Files (*.xls,*.xlsx)|*.xls;*.xlsx";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string fileOpened = openFileDialog1.FileName;
+                try
+                {
+                    filePath.Text = fileOpened;
+                    filePath.Refresh();
+                    var worksheets = Workbook.Worksheets(fileOpened);
+                    var firstWorksheet = worksheets.FirstOrDefault();
+                    _rows = firstWorksheet.Rows;
+
+                    if (_rows != null && _rows.Length > 1)
+                    {
+                        _rowMax = _rows.Length - 1;
+                        _colMax = _rows[0].Cells.Length - 1;
+                        nextButton_Click(this, e);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            if (_rowCount >= _rowMax && _colCount >= _colMax)
+            {
+                nextButton.Enabled = false;
+            }
+
+            var thisRow = _rows[_rowCount];
+            var thisCol = thisRow.Cells[_colCount];
+
+            cellValue.Text = thisCol.Text;
+            cellValue.SelectAll();
+            cellValue.Focus();
+
+            if (_colCount < _colMax)
+            {
+                _colCount++;
+            }
+            else
+            {
+                _colCount = 0;
+                _rowCount++;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
